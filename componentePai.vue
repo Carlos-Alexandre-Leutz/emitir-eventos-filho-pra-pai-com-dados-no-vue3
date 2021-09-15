ad886c72971b8cbb62da49e5f59d91305729e37d
Carlos Alexandre Leutz <54986289+Carlos-Alexandre-Leutz@users.noreply.github.com>
2021-09-15 11:01:12 -0300

    Create componentePai.vue

---
+++ componentePai.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <Filho v-on:nomeDoMEuEvento="recebeEvento">
+    </Filho>
+  </div>
+</template>
+<script>
+
+</script>
+import Filho from "./componenteFilho";
+export default {
+    components: {
+    Filho
+    
+    },
+    setup() {
+    const recebeEvento = (e) =>{
+       console.log("data do componente filho" , e)
+    
+    }
+    return {recebeEvento}
+    
+  }
