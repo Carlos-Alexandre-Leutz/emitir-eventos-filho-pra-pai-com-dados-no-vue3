59aee7c164385709caab55a64943d139c3ef5d7d
Carlos Alexandre Leutz <54986289+Carlos-Alexandre-Leutz@users.noreply.github.com>
2021-09-15 10:57:05 -0300

    Create componenteFilho.vue

---
+++ componenteFilho.vue
@@ -0,0 +1,15 @@
+<template>
+  <div> 
+    <button @click="minhaFuncao">adicionar evento </button>
+  </div>
+</template>
+<script>
+export default {
+ methods: {
+  minhaFuncao(){
+    let data = "conteudo"; 
+    this.$emit("nomeDoMEuEvento", data);
+  }
+ }
+}
+</script>
